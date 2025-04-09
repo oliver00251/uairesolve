69019ed3d645af82c93e1ff0c8f26ab4be12587b
oliver00251 <natan.candido.dev@gmail.com>
2025-04-09 10:30:04 -0300

    ajsute exibição ocorrencia

--- resources/views/ocorrencias/index.blade.php
+++ resources/views/ocorrencias/index.blade.php
@@ -15,6 +15,10 @@
                 @forelse($ocorrencias as $ocorrencia)
                     <div class="col-12 col-md-3 mb-3">
                         <div class="card ocorrencia-card h-100">
+                            <div class="ocorrencia-capa-wrapper">
+                                <img src="{{ asset('storage/' . $ocorrencia->imagem) }}"
+                                     class="ocorrencia-capa">
+                            </div>
                             <div class="card-body d-flex flex-column justify-content-between">
                                 <!-- Cabeçalho: Categoria e Status -->
                                 <div class="d-flex justify-content-between align-items-center mb-2">
@@ -135,4 +139,17 @@ a.btn.btn-add-ocorrencia {
     border-radius: 50%;
     box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.2);
 }
+.ocorrencia-capa-wrapper {
+    height: 180px;
+    overflow: hidden;
+    border-top-left-radius: 0.375rem;  /* se quiser combinar com o card */
+    border-top-right-radius: 0.375rem;
+}
+
+.ocorrencia-capa {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+}
+
 </style>
