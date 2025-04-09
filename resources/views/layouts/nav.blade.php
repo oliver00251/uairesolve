3f52c7dcc9a90c9f281df54a54445ef7574c1184
oliver00251 <natan.candido.dev@gmail.com>
2025-04-09 18:56:44 -0300

    ajuste home

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -9,45 +9,52 @@
             <span class="navbar-toggler-icon"></span>
         </button>
 
-      
-       <!-- Menu desktop -->
-<div class="collapse navbar-collapse justify-content-end d-none d-lg-flex" id="navbarNav">
-    <ul class="navbar-nav align-items-center gap-3">
-        <li class="nav-item">
-            <a class="nav-link" href="{{ route('ocorrencias.index') }}">
-                <i class="fas fa-map-marker-alt text-primary"></i> Publicações
-            </a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="{{ route('vagas.index') }}">
-                <i class="fas fa-briefcase text-primary"></i> Vagas de Emprego
-            </a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="https://www.instagram.com/uairesolveoficial/" target="_blank">
-                <i class="fab fa-instagram text-primary"></i> Fale Conosco
-            </a>
-        </li>
-        <li class="nav-item dropdown">
-            <button class="btn btn-outline-primary dropdown-toggle" type="button" id="userMenu" data-bs-toggle="dropdown" aria-expanded="false">
-                <i class="fas fa-user-circle"></i> {{ Auth::user()->name }}
-            </button>
-            <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="userMenu">
-                <li><a class="dropdown-item" href="/dashboard"><i class="fas fa-user"></i> Meu Perfil</a></li>
-                <li><hr class="dropdown-divider"></li>
-                <li>
-                    <form action="{{ route('logout') }}" method="POST">
-                        @csrf
-                        <button type="submit" class="dropdown-item text-danger">
-                            <i class="fas fa-sign-out-alt"></i> Sair
-                        </button>
-                    </form>
+        <!-- Menu desktop -->
+        <div class="collapse navbar-collapse justify-content-end d-none d-lg-flex" id="navbarNav">
+            <ul class="navbar-nav align-items-center gap-3">
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('ocorrencias.index') }}">
+                        <i class="fas fa-map-marker-alt text-primary"></i> Publicações
+                    </a>
                 </li>
-            </ul>
-        </li>
-    </ul>
-</div>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('vagas.index') }}">
+                        <i class="fas fa-briefcase text-primary"></i> Vagas de Emprego
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="https://www.instagram.com/uairesolveoficial/" target="_blank">
+                        <i class="fab fa-instagram text-primary"></i> Fale Conosco
+                    </a>
+                </li>
 
+                @auth
+                <li class="nav-item dropdown">
+                    <button class="btn btn-outline-primary dropdown-toggle" type="button" id="userMenu" data-bs-toggle="dropdown" aria-expanded="false">
+                        <i class="fas fa-user-circle"></i> {{ Auth::user()->name }}
+                    </button>
+                    <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="userMenu">
+                        <li><a class="dropdown-item" href="/dashboard"><i class="fas fa-user"></i> Meu Perfil</a></li>
+                        <li><hr class="dropdown-divider"></li>
+                        <li>
+                            <form action="{{ route('logout') }}" method="POST">
+                                @csrf
+                                <button type="submit" class="dropdown-item text-danger">
+                                    <i class="fas fa-sign-out-alt"></i> Sair
+                                </button>
+                            </form>
+                        </li>
+                    </ul>
+                </li>
+                @else
+                <li class="nav-item">
+                    <a href="{{ route('login') }}" class="btn btn-outline-primary">
+                        <i class="fas fa-sign-in-alt"></i> Entrar
+                    </a>
+                </li>
+                @endauth
+            </ul>
+        </div>
     </div>
 </nav>
 
